b813a43c04819bcfbbc2ffd8ecf09253e5e5bde4
pandazheng <543828315@qq.com>
2013-06-20 07:17:33 -0700

    Piano
    Piano

--- piano/ViewController.h
+++ piano/ViewController.h
@@ -7,7 +7,30 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import <Foundation/Foundation.h>
 
 @interface ViewController : UIViewController
+{
+    NSString *soundFile;
+}
+
+@property (nonatomic,strong) NSString *soundFile;
+
+- (void) playSound: (NSString*) soundKey;
+
+- (IBAction)DO:(id)sender;
+- (IBAction)RE:(id)sender;
+- (IBAction)MI:(id)sender;
+- (IBAction)FA:(id)sender;
+- (IBAction)SO:(id)sender;
+- (IBAction)LA:(id)sender;
+- (IBAction)SI:(id)sender;
+
+- (IBAction)C:(id)sender;
+- (IBAction)D:(id)sender;
+- (IBAction)E:(id)sender;
+- (IBAction)F:(id)sender;
+- (IBAction)G:(id)sender;
 
 @end
--- piano/ViewController.m
+++ piano/ViewController.m
@@ -13,6 +13,101 @@
 @end
 
 @implementation ViewController
+@synthesize soundFile;
+
+- (void) playSound:(NSString *)soundKey
+{
+    NSURL *SoundUrl = [[NSBundle mainBundle] URLForResource:soundKey withExtension:@"mp3"];
+    
+    SystemSoundID soundID;
+    
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef)SoundUrl, &soundID);
+    AudioServicesPlaySystemSound(soundID);
+}
+
+- (IBAction) DO: (id)sender
+{
+    soundFile = [NSString stringWithFormat:@"001"];
+    
+    [self playSound:soundFile];
+}
+
+- (IBAction) RE: (id)sender
+{
+    soundFile = [NSString stringWithFormat:@"002"];
+    
+    [self playSound:soundFile];
+}
+
+- (IBAction) MI:(id)sender
+{
+    soundFile = [NSString stringWithFormat:@"003"];
+    
+    [self playSound:soundFile];
+}
+
+- (IBAction) FA:(id)sender
+{
+    soundFile = [NSString stringWithFormat:@"004"];
+    
+    [self playSound:soundFile];
+}
+
+- (IBAction) SO:(id)sender
+{
+    soundFile = [NSString stringWithFormat:@"005"];
+    
+    [self playSound:soundFile];
+}
+
+- (IBAction) LA:(id)sender
+{
+    soundFile = [NSString stringWithFormat:@"006"];
+    
+    [self playSound:soundFile];
+}
+
+- (IBAction) SI:(id)sender
+{
+    soundFile = [NSString stringWithFormat:@"007"];
+    
+    [self playSound:soundFile];
+}
+
+- (IBAction) C:(id)sender
+{
+    soundFile = [NSString stringWithFormat:@"C"];
+    
+    [self playSound:soundFile];
+}
+
+- (IBAction) D:(id)sender
+{
+    soundFile = [NSString stringWithFormat:@"D"];
+    
+    [self playSound:soundFile];
+}
+
+- (IBAction) E:(id)sender
+{
+    soundFile = [NSString stringWithFormat:@"E"];
+    
+    [self playSound:soundFile];
+}
+
+- (IBAction) F:(id)sender
+{
+    soundFile = [NSString stringWithFormat:@"F"];
+    
+    [self playSound:soundFile];
+}
+
+- (IBAction) G:(id)sender
+{
+    soundFile = [NSString stringWithFormat:@"G"];
+    
+    [self playSound:soundFile];
+}
 
 - (void)viewDidLoad
 {
