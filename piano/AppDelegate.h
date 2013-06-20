995ba04fbe7b60043ccf574f22fd2aea0a53c36b
pandazheng <543828315@qq.com>
2013-06-20 06:01:36 -0700

    Initial Commit

---
+++ piano/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  piano
+//
+//  Created by panda zheng on 13-6-20.
+//  Copyright (c) 2013年 panda zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ piano/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  piano
+//
+//  Created by panda zheng on 13-6-20.
+//  Copyright (c) 2013年 panda zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
